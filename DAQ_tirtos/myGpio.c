9b28534f96617dec6b1df5542cb9affc3f28f5a9
r3n3iii <ayoroa.rene@gmail.com>
2022-11-19 01:11:48 -0300

    fixr: Console task blocking Idle task

--- DAQ_tirtos/myGpio.c
+++ DAQ_tirtos/myGpio.c
@@ -48,7 +48,7 @@ void myGpio_init(void)
     GPIO_setConfig(CONFIG_GPIO_LED_3, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW);     // Configure "LED_3" as an output and set it low (i.e. '0')
 
 
-    GPIO_setConfig(CONFIG_GPIO_BUTTON_1,                                        // Configure I/O pin for Button1
+    GPIO_setConfig(CONFIG_GPIO_BUTTON_0|CONFIG_GPIO_BUTTON_1,                                        // Configure I/O pin for Button1
                    GPIO_CFG_IN_PU                                               // Configure as input pin with internal pull-up
                    | GPIO_CFG_IN_INT_FALLING                                    // Configure interrupt to trigger on falling edge
     );
@@ -57,29 +57,14 @@ void myGpio_init(void)
 
 
     // Install button interrupt callback functions
-
+    GPIO_setCallback(CONFIG_GPIO_BUTTON_0, gpioButtonFxn0);                     // When "SW2" is pressed, call 'gpioButtonFxn1'
     GPIO_setCallback(CONFIG_GPIO_BUTTON_1, gpioButtonFxn1);                     // When "SW2" is pressed, call 'gpioButtonFxn1'
 
     // Enable interrupts
 
+    GPIO_enableInt(CONFIG_GPIO_BUTTON_0);  // Enable individual interrupt for Board_GPIO_BUTTON1
     GPIO_enableInt(CONFIG_GPIO_BUTTON_1);  // Enable individual interrupt for Board_GPIO_BUTTON1
-
     printf("GPIO has been initialized\n");
-}
-//*****************************************************************************
-// gpioButtonFxn0
-//
-// Callback function for the GPIO interrupt on Board_GPIO_BUTTON0
-//
-// Notes:
-//   - CONFIG_GPIO_BUTTON_0 is connected to Switch 1 (SW1) on the LaunchPad
-//   - The "index" argument is not used by this function, but it references
-//     which GPIO input was triggered as defined by the GPIOName provided
-//     in the project's board specific header file
-//*****************************************************************************
-void gpioButtonFxn0(uint_least8_t index)
-{
-    GPIO_toggle(CONFIG_GPIO_LED_0);                                             // Toggle LED
 }
 
 //*****************************************************************************
